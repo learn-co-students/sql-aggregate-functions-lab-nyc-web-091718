6e7891e94c18cdb30c2e9f7950d50593eb59772a
github username <github email address>
2018-09-27 11:46:13 -0400

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,30 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+#require 'pry'
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
+SELECT MAX (gpa)
+FROM students;
 end
+#SELECT student_name, major, gpa
+#FROM   student
+#WHERE  gpa IN (SELECT MAX(gpa) FROM student)
 
 def lowest_student_gpa
+  SELECT MAX (gpa)
+  FROM students;
 end
 
 def average_student_gpa
+  SELECT AVG (gpa)
+  FROM students;
 end
 
 def total_tardies_for_all_students
+  SELECT SUM (tardies)
+  students;
 end
 
 def average_gpa_for_9th_grade
+  SELECT AVG (gpa)
+  FROM students WHERE grade = 9;
 end

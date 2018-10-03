c2c40fd5e7299262f22c673a54cc47bb1c751640
github username <github email address>
2018-10-02 22:35:39 -0400

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,21 +1,39 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
+  #{}"SELECT gpa FROM students ORDER BY gpa DESC LIMIT 1;"
   "SELECT MAX(gpa) FROM students;"
 end
 
+
 def lowest_student_gpa
+  #{}"SELECT gpa FROM students ORDER BY gpa LIMIT 1;"
   "SELECT MIN(gpa) FROM students;"
 end
 
+
 def average_student_gpa
-  "SELECT AVG(gpa) FROM students"
+  "SELECT AVG(gpa) FROM students;"
 end
+
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) FROM students"
+  "SELECT SUM(tardies) from students;"
 end
 
+
 def average_gpa_for_9th_grade
-  "SELECT AVG(gpa) FROM students WHERE grade = 9"
+  "SELECT AVG(gpa) FROM students WHERE grade = 9;"
 end
+
+# aggregate functions sql
+#   #highest_student_gpa
+#     finds the highest gpa
+#   #lowest_student_gpa
+#     finds the lowest gpa
+#   #average_student_gpa
+#     finds the average gpa
+#   #total_tardies_for_all_students
+#     finds the total amount of tardies for all students
+#   #average_gpa_for_9th_grade
+#     find the average gpa for 9th grade
